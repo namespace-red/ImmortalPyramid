0c26d7cf26dc2d59c3f74e08a84319c14fd3c724
namespace-red <67328203+namespace-red@users.noreply.github.com>
2023-09-25 22:11:51 +0300

    Player animations
    Created animations and controller.

--- Assets/Scripts/Animations/PlayerAnimationsController.cs
+++ Assets/Scripts/Animations/PlayerAnimationsController.cs
@@ -1,39 +1,56 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Animator))]
-public class PlayerAnimationsController : MonoBehaviour
+[RequireComponent(typeof(Health))]
+public class PlayerAnimationsController : MonoBehaviour, IAttackAnimation,
+    IMovementAnimation, IDeathAnimation
 {
     private Animator _animator;
+    private Health _health;
     
-    void Start()
+    private void Awake()
     {
         _animator = GetComponent<Animator>();
+        _health = GetComponent<Health>();
     }
 
     private void OnEnable()
     {
+        _health.Died += PlayDeath;
     }
 
     private void OnDisable()
     {
+        _health.Died -= PlayDeath;
     }
 
-    
-    private void OnAttacked() => _animator.SetTrigger(States.Attack);
     // private void OnJumped() => _animator.SetBool(Params.IsJumping, true);
-    
     // private void OnGrounded() => _animator.SetBool(Params.IsJumping, false);
-    
+ 
+    public void PlayAttack()
+    {
+        _animator.SetTrigger(States.Shoot);
+    }
+
+    public void SetMoveState(bool state)
+    {
+         _animator.SetBool(Params.IsRuning, state);
+    }
+
+    public void PlayDeath()
+    {
+        _animator.SetTrigger(States.Died);
+    }
+
     private static class Params
     {
         public const string IsRuning = nameof(IsRuning);
         // public const string IsJumping = nameof(IsJumping);
     }
-    
+
     private static class States
     {
-        public const string Attack = nameof(Attack);
+        public const string Shoot = nameof(Shoot);
         public const string Died = nameof(Died);
-        
     }
 }
