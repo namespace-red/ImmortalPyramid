486c73c12a4fdc5ba7d43f87be668a9866de4587
namespace-red <67328203+namespace-red@users.noreply.github.com>
2023-09-30 20:28:59 +0300

    EnemyFactory
    Created EnemyFactory, moved prefabs to Resource folder.

---
+++ Assets/Scripts/Factory/EnemyFactory.cs
@@ -0,0 +1,42 @@
+using System;
+using UnityEngine;
+
+public class EnemyFactory : MonoBehaviour
+{
+    [SerializeField] private GameObject _targetObjContainer;
+    [SerializeField] private Transform _spawnPoint;
+    
+    private const string Folder = "Prefabs/";
+    private ITargetWithHeathData _target;
+
+    private void Start()
+    {
+        _targetObjContainer = _targetObjContainer ?? throw new ArgumentException("EnemyFactory._targetObjContainer can't be null");
+        
+        if (_targetObjContainer.TryGetComponent<ITargetWithHeathData>(out _target) == false)
+            throw new ArgumentException("EnemyFactory._targetObjContainer doesn't have ITargetWithHeathData");
+        
+        _spawnPoint = _spawnPoint ?? throw new ArgumentException("EnemyFactory._spawnPoint can't be null");
+    }
+
+    public Enemy Create(EnemyType enemyType)
+    {
+        switch (enemyType)
+        {
+            case EnemyType.Minotaur:
+                return CreateMinotaur();
+            
+            default:
+                throw new ArgumentException("Not correct EnemyType");
+        }
+    }
+    
+    public Enemy CreateMinotaur()
+    {
+        var prifab = Resources.Load<GameObject>( Folder + "Minotaur");
+        var newGameObject = Instantiate(prifab, _spawnPoint);
+        var minotaur = newGameObject.GetComponent<Minotaur>();
+        minotaur.Init(_target);
+        return minotaur;
+    }
+}
